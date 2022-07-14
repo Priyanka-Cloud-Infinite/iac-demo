6c5e45393fa3514b51cf9578366171db071d1be5
Priyanka Patel <83409681+Priyanka-Cloud-Infinite@users.noreply.github.com>
2022-07-14 18:49:09 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -29,12 +29,22 @@ resource "aws_security_group" "my_ec2_sg" {
   Name="Ec2-SG"
   }
 }
+resource "tls_private_key" "key" {
+ algorithm = "RSA"
+ rsa_bits  = 4096
+}
+ 
+resource "aws_key_pair" "aws_key" {
+ key_name   = "bitops-ssh-key"
+ public_key = tls_private_key.key.public_key_openssh
+}
 
 resource "aws_instance" "My-instance" {
   ami                         = "${var.ami_id}"
   subnet_id                   = "subnet-06762400af542a38c"
   instance_type               = "${var.instance_type}"
+  key_name                    = aws_key_pair.aws_key.key_name
   associate_public_ip_address = true
   security_groups             = [aws_security_group.my_ec2_sg.id]
-   #key_name =  "${var.keyname}"
+  
 }
